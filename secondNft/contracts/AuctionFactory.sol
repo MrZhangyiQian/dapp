d77440595ed36dc30eceb6d6c9182ed00b467471
MrZhangyiQian <99602928+MrZhangyiQian@users.noreply.github.com>
2025-09-09 22:19:16 +0800

    Update AuctionFactory.sol

--- secondNft/contracts/AuctionFactory.sol
+++ secondNft/contracts/AuctionFactory.sol
@@ -2,32 +2,42 @@
 pragma solidity ^0.8.20;
 
 import "./Auction.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-//创建拍卖合约的工厂合约
-contract AuctionFactory {
-    // 存储所有创建的拍卖合约地址
+contract AuctionFactory is Ownable {
     address[] public auctions;
-    // 用于记录某个地址是否为拍卖合约
     mapping(address => bool) public isAuction;
-    
-    // 当拍卖合约被创建时触发，记录拍卖合约地址、卖家地址和 NFT 合约地址
-    event AuctionCreated(address indexed auction, address indexed seller, address indexed nftContract);
-    
-    // 创建一个新的拍卖合约
+
+    event AuctionCreated(
+        address indexed auction,
+        address indexed seller,
+        address indexed nftContract
+    );
+
+    constructor() Ownable(msg.sender) {}
+
     function createAuction(
-       address _nftContract,
-       uint256 _tokenId,
-       uint256 _duration,
-       address _priceFeed
-    ) external returns (address) {
-        // 部署一个新的 Auction 逻辑合约。
-       Auction auctionLogic = new Auction();
-       auctionLogic.initialize(msg.sender, _nftContract, _tokenId, _duration, _priceFeed);
-        // 触发事件，记录拍卖合约信息。
+        address _nftContract,
+        uint256 _tokenId,
+        uint256 _duration,
+        address _priceFeed
+    ) external onlyOwner returns (address) {
+       
+        Auction auctionLogic = new Auction();
+        auctionLogic.initialize(
+            msg.sender,
+            _nftContract,
+            _tokenId,
+            _duration,
+            _priceFeed
+        );
+        address auctionAddress = address(auctionLogic);
+        auctions.push(auctionAddress);
+        isAuction[auctionAddress] = true;
         emit AuctionCreated(address(auctionLogic), msg.sender, _nftContract);
         return address(auctionLogic);
-}
-    
+    }
+
     function getAllAuctions() external view returns (address[] memory) {
         return auctions;
     }
