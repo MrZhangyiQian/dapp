159ba5b02831b4cba042660b16ef0293ff9920ed
MrZhangyiQian <99602928+MrZhangyiQian@users.noreply.github.com>
2025-09-09 22:19:35 +0800

    Update IAuction.sol

--- secondNft/contracts/interfaces/IAuction.sol
+++ secondNft/contracts/interfaces/IAuction.sol
@@ -1,27 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// 拍卖合约接口
 interface IAuction {
     function initialize(
-        // 卖家
         address _seller,
-        // NFT合约地址
         address _nftContract,
-        // 代币ID
         uint256 _tokenId,
-        // 拍卖持续时间
         uint256 _duration,
-        // 价格预言机地址
         address _priceFeed
     ) external payable;
-    // 参与竞价
-    function placeBid() external payable;
-    //  结束拍卖
+    
+    function placeBid(uint256 _amount) external payable;
     function endAuction() external;
-    // 获取ETH对USD的当前价格
     function getEthUsdPrice() external view returns (uint256);
-    // 将ETH金额转换为USD价值
     function getBidUsdValue(uint256 ethAmount) external view returns (uint256);
 
     // ✅ 添加 getter 函数声明
@@ -30,6 +21,7 @@ interface IAuction {
         view
         returns (
             address seller,
+            address originalOwner,
             address nftContract,
             uint256 tokenId,
             uint256 startTime,
